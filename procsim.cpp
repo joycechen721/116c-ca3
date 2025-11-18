ac833c646f3ec284eefab66cb8ed00e4abe8de80
Joyce Chen <joyce.chen721@gmail.com>
2025-11-17 22:10:29 -0800

    fetch and dispatch stage done

--- procsim.cpp
+++ procsim.cpp
@@ -1,4 +1,42 @@
 #include "procsim.hpp"
+#include <deque>
+#include <vector>
+
+// global processor states
+uint64_t RESULT_BUSES;
+uint64_t K0_FU_COUNT;
+uint64_t K1_FU_COUNT;
+uint64_t K2_FU_COUNT;
+uint64_t FETCH_RATE;
+
+// dispatch queue
+std::deque<proc_inst_t> dispatch_queue;
+
+// reservation station
+std::deque<rs_entry_t> reservation_station;
+
+// register ready table
+const int32_t NUM_REGISTERS = 128;
+bool register_ready[NUM_REGISTERS];
+
+// FU ready counts
+uint64_t k0_fu_available;
+uint64_t k1_fu_available;
+uint64_t k2_fu_available;
+
+// result bus ready count
+uint64_t result_buses_available;
+
+// global counters
+uint64_t global_tag_counter = 1;
+uint64_t current_cycle = 0;
+
+// statistics tracking
+uint64_t max_disp_size = 0;
+uint64_t total_disp_size = 0;
+uint64_t disp_size_count = 0;
+uint64_t instructions_fired = 0;
+uint64_t instructions_retired = 0;
 
 /**
  * Subroutine for initializing the processor. You many add and initialize any global or heap
@@ -13,7 +51,34 @@
  */
 void setup_proc(uint64_t r, uint64_t k0, uint64_t k1, uint64_t k2, uint64_t f) 
 {
+  RESULT_BUSES = r; // number of result buses
+  K0_FU_COUNT = k0; // number of k0 functional units
+  K1_FU_COUNT = k1; // number of k1 functional units
+  K2_FU_COUNT = k2; // number of k2 functional units
+  FETCH_RATE = f;   // number of instructions to fetch
 
+  dispatch_queue.clear();
+  reservation_station.clear();
+
+  // initialize reservation station
+  uint64_t rs_size = 2 * (K0_FU_COUNT + K1_FU_COUNT + K2_FU_COUNT);
+  reservation_station.resize(rs_size);
+  for (auto& entry : reservation_station) {
+      entry.valid = false;
+  }
+  // initialize register ready table
+  for (int32_t i = 0; i < NUM_REGISTERS; i++) {
+      register_ready[i] = true; // all registers are initially ready
+  }
+
+  // initialize FU ready counts
+  k0_fu_available = K0_FU_COUNT;
+  k1_fu_available = K1_FU_COUNT;
+  k2_fu_available = K2_FU_COUNT;
+
+  // initialize result bus ready count
+  result_buses_available = RESULT_BUSES;
+  printf("CYCLE	OPERATION	INSTRUCTION\n");
 }
 
 /**
@@ -25,7 +90,16 @@ void setup_proc(uint64_t r, uint64_t k0, uint64_t k1, uint64_t k2, uint64_t f)
  */
 void run_proc(proc_stats_t* p_stats)
 {
-
+  bool done = false;
+  
+  while (!done) {
+    current_cycle++;    
+    // state_update_stage();
+    // execute_stage();
+    // schedule_stage();
+    dispatch_stage();
+    fetch_stage();
+  }
 }
 
 /**
@@ -38,4 +112,47 @@ void run_proc(proc_stats_t* p_stats)
 void complete_proc(proc_stats_t *p_stats) 
 {
 
+}
+
+// FETCH STAGE
+void fetch_stage() {
+  for (uint64_t i = 0; i < FETCH_RATE; i++) {
+    proc_inst_t inst;
+    if (read_instruction(&inst)) {
+      inst.tag = global_tag_counter++;
+      printf("%llu\tFETCHED\t\t%llu\n", current_cycle, inst.tag);
+      dispatch_queue.push_back(inst);
+    } else {
+      break;
+    }
+  }
+}
+
+// DISPATCH STAGE
+void dispatch_stage() {
+  uint64_t rs_size = 2 * (K0_FU_COUNT + K1_FU_COUNT + K2_FU_COUNT);
+  while (!dispatch_queue.empty()) {
+    // find empty slot in RS
+    int empty_slot = -1;
+    for (uint64_t i = 0; i < rs_size; i++) {
+      if (!reservation_station[i].valid) {
+        empty_slot = i;
+        break;
+      }
+    }
+    // move instruction from dispatch queue to RS
+    if (empty_slot != -1) {
+      proc_inst_t inst = dispatch_queue.front();
+      dispatch_queue.pop_front();
+
+      rs_entry_t rs_entry;
+      rs_entry.instruction = inst;
+      rs_entry.ready_to_fire = false;
+      rs_entry.fired = false;
+      rs_entry.completed = false;
+      rs_entry.execute_cycles_left = 0;
+      reservation_station[empty_slot] = rs_entry;
+      printf("%llu\tDISPATCHED\t%llu\n", current_cycle, inst.tag);
+    }
+  }
 }
--- procsim.hpp
+++ procsim.hpp
@@ -3,6 +3,7 @@
 
 #include <cstdint>
 #include <cstdio>
+#include <vector>
 
 #define DEFAULT_K0 1
 #define DEFAULT_K1 2
@@ -14,11 +15,9 @@ typedef struct _proc_inst_t
 {
     uint32_t instruction_address;
     int32_t op_code;
-    int32_t src_reg[2];
     int32_t dest_reg;
-    
-    // You may introduce other fields as needed
-    
+    int32_t src_reg[2];
+    uint64_t tag;
 } proc_inst_t;
 
 typedef struct _proc_stats_t
@@ -31,10 +30,32 @@ typedef struct _proc_stats_t
     unsigned long cycle_count;
 } proc_stats_t;
 
+typedef struct dispatch_entry_t {
+    proc_inst_t instruction;
+    bool dispatched;
+} dispatch_entry_t;
+
+typedef struct rs_entry_t {
+    bool valid;
+    proc_inst_t instruction;
+    bool ready_to_fire;
+    bool fired;
+    bool completed;
+    uint64_t execute_cycles_left;
+} rs_entry_t;
+
 bool read_instruction(proc_inst_t* p_inst);
 
 void setup_proc(uint64_t r, uint64_t k0, uint64_t k1, uint64_t k2, uint64_t f);
 void run_proc(proc_stats_t* p_stats);
 void complete_proc(proc_stats_t* p_stats);
+
+void fetch_stage();
+void dispatch_stage();
+void schedule_stage();
+void execute_stage();
+void state_update_stage();
+
+bool all_rs_empty();
 
 #endif /* PROCSIM_HPP */
